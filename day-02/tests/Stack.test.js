962f3d3c3f35c7eb8682d2d01ead8e10d116ce64
kathrynzaro <zarokathryn@gmail.com>
2022-09-20 16:29:36 -0700

    adds test for all test cases / refactors existing tests

--- day-02/tests/Stack.test.js
+++ day-02/tests/Stack.test.js
@@ -4,17 +4,34 @@ describe('Stack', () => {
   it('push should add item to back', () => {
     const stack = new Stack(['fox', 'goose', 'lizard']);
     stack.push('cat');
-    expect(stack.peek()).toEqual('cat');
+    expect(stack.list).toEqual(['fox', 'goose', 'lizard', 'cat']);
   });
 
   it('pop should remove item from back', () => {
     const stack = new Stack(['fox', 'goose', 'lizard']);
     stack.pop();
-    expect(stack.peek()).toEqual('goose');
+    expect(stack.list).toEqual(['fox', 'goose']);
   });
 
   it('peek returns item at the end', () => {
     const stack = new Stack(['fox', 'goose', 'lizard', 'llama']);
     expect(stack.peek()).toEqual('llama');
+  });
+
+  it('test cases', () => {
+    const stack = new Stack();
+    stack.push('fox');
+    stack.push('goose');
+    stack.push('lizard');
+    stack.pop();
+    expect(stack.peek()).toEqual('goose');
+    stack.pop();
+    expect(stack.list).toEqual(['fox']);
+    stack.push('llama');
+    expect(stack.list).toEqual(['fox', 'llama']);
+    stack.pop();
+    expect(stack.peek()).toEqual('fox');
+    stack.pop();
+    expect(stack.list).toEqual([]);
   });
 });
