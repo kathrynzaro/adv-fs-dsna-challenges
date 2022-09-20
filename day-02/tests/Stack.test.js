a7495a18f3f5f8c7f29e610564968559fc402ac4
kathrynzaro <zarokathryn@gmail.com>
2022-09-20 16:17:42 -0700

    peek returns last item

--- day-02/tests/Stack.test.js
+++ day-02/tests/Stack.test.js
@@ -2,14 +2,19 @@ const { Stack } = require('../Stack.js');
 
 describe('Stack', () => {
   it('push should add item to back', () => {
-    const stack = new Stack();
-    stack.push('fox');
-    expect(stack.peek()).toEqual('fox');
+    const stack = new Stack(['fox', 'goose', 'lizard']);
+    stack.push('cat');
+    expect(stack.peek()).toEqual('cat');
   });
 
   it('pop should remove item from back', () => {
     const stack = new Stack(['fox', 'goose', 'lizard']);
     stack.pop();
     expect(stack.peek()).toEqual('goose');
+  });
+
+  it('peek returns item at the end', () => {
+    const stack = new Stack(['fox', 'goose', 'lizard', 'llama']);
+    expect(stack.peek()).toEqual('llama');
   });
 });
