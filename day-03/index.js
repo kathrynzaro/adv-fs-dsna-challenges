65d7ea389fa3786468a54d46f0cc06819f2176f4
kathrynzaro <zarokathryn@gmail.com>
2022-09-22 17:50:23 -0700

    person tree node

--- day-03/index.js
+++ day-03/index.js
@@ -25,4 +25,49 @@ class BinaryTreeNode {
   }
 }
 
-module.exports = { BinaryTreeNode };
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    if (node.value === this.value) {
+      return null;
+    }
+    if (node.value < this.value) {
+      if (this.left == null) {
+        this.left = node;
+      } else {
+        this.left.add(node);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = node;
+      } else {
+        this.right.add(node);
+      }
+    }
+  }
+
+  findPerson(name) {
+    if (name === this.value) return this.person;
+    if (name < this.value) {
+      if (this.left == null) {
+        return null;
+      } else {
+        return this.left.findPerson(name);
+      }
+    } else {
+      if (this.right == null) {
+        return null;
+      } else {
+        return this.right.findPerson(name);
+      }
+    }
+  }
+}
+
+module.exports = { BinaryTreeNode, PersonTreeNode };
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,4 +1,4 @@
-const { BinaryTreeNode } = require('.');
+const { BinaryTreeNode, PersonTreeNode } = require('.');
 
 describe('node based data structures', () => {
   it('returns a binary tree and adds nodes', () => {
@@ -14,5 +14,23 @@ describe('node based data structures', () => {
     expect(B.left.value).toEqual('A');
     expect(B.right.value).toBe('D');
     expect(B.right.left.value).toBe('C');
+  });
+
+  it('takes a name and returns that person object', () => {
+    const Jones = new PersonTreeNode({ name: 'Jones' });
+    const Brook = new PersonTreeNode({ name: 'Brook' });
+    const Smith = new PersonTreeNode({ name: 'Smith' });
+    const Nelson = new PersonTreeNode({ name: 'Nelson' });
+
+    Jones.add(Brook);
+    Jones.add(Smith);
+    Jones.add(Nelson);
+
+    expect(Jones.findPerson('Brook')).toEqual({ name: 'Brook' });
+    expect(Jones.findPerson('Smith')).toEqual({ name: 'Smith' });
+
+    expect(Jones.left.value).toEqual('Brook');
+    expect(Jones.right.value).toEqual('Smith');
+    expect(Jones.right.left.value).toEqual('Nelson');
   });
 });
